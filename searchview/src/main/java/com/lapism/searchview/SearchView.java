4ac030393a3ca76b703e67522f0528a0f0d8de40
Ivam Luz <iluz@ciandt.com>
2016-07-02 09:36:46 -0300

    Fix search disappearing after toggling from MENU version to either TOOLBAR or TOOLBAR_ICON versions

--- searchview/src/main/java/com/lapism/searchview/SearchView.java
+++ searchview/src/main/java/com/lapism/searchview/SearchView.java
@@ -353,15 +353,18 @@ public class SearchView extends FrameLayout implements View.OnClickListener {
             mEditText.clearFocus();
             mSearchArrow = new SearchArrowDrawable(mContext);
 
+            mBackImageView.setImageDrawable(mSearchArrow);
+
             setVisibility(View.VISIBLE);
             mCardView.setVisibility(VISIBLE);
-
-            mBackImageView.setImageDrawable(mSearchArrow);
         }
 
         if (mVersion == VERSION_TOOLBAR_ICON) {
             mEditText.clearFocus();
             mBackImageView.setImageResource(R.drawable.search_ic_arrow_back_black_24dp);
+
+            setVisibility(View.VISIBLE);
+            mCardView.setVisibility(VISIBLE);
         }
 
         if (mVersion == VERSION_MENU_ITEM) {
