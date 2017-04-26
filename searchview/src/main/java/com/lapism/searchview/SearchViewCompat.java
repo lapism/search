a26765581ea1ef5bbec5b58da62288000535f8cf
Martin Lapiš <martin.lapis@firma.seznam.cz>
2017-04-26 12:29:00 +0200

    update3

--- searchview/src/main/java/com/lapism/searchview/SearchViewCompat.java
+++ searchview/src/main/java/com/lapism/searchview/SearchViewCompat.java
@@ -1,6 +1,11 @@
 package com.lapism.searchview;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
 /**
@@ -9,4 +14,22 @@ import android.widget.FrameLayout;
 
 @CoordinatorLayout.DefaultBehavior(SearchBehavior.class)
 public class SearchViewCompat extends FrameLayout {
+
+    public SearchViewCompat(@NonNull Context context) {
+        super(context);
+    }
+
+    public SearchViewCompat(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SearchViewCompat(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(21)
+    public SearchViewCompat(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
 }
