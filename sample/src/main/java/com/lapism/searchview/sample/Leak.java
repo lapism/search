4a990dad0c738effbbf2989cc143de54c86ffcea
Martin Lapiš <lapis.martin@seznam.cz>
2017-05-03 20:48:19 +0200

    push

--- sample/src/main/java/com/lapism/searchview/sample/Leak.java
+++ sample/src/main/java/com/lapism/searchview/sample/Leak.java
@@ -10,7 +10,13 @@ public class Leak extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
         LeakCanary.install(this);
+        // Normal app init code...
     }
 
 }
