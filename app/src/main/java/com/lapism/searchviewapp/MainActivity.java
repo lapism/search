0f5af1fe31b77657d204fe4c381b861436a195dd
Emmanuel <emmanuelbendavid@gmail.com>
2017-12-03 23:19:35 -0300

    Modularized library and created test app

---
+++ app/src/main/java/com/lapism/searchviewapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.lapism.searchviewapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
