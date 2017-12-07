c7f00aa312c82c525bbbc6a1b8fcaa7b0b53271e
Martin Lapiš <martin.lapis@firma.seznam.cz>
2017-12-07 18:15:52 +0100

    da

--- src/main/java/com/lapism/searchview/SearchAdapter.java
+++ src/main/java/com/lapism/searchview/SearchAdapter.java
@@ -21,7 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-@SuppressWarnings({"WeakerAccess", "unused"})
+
 public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ResultViewHolder> implements Filterable {
 
     protected final SearchHistoryTable mHistoryDatabase;
